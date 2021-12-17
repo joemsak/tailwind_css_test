71f8c7daa6ac9a6d324ba4b122e61b19842bc117
Joe Sak <joe@joesak.com>
2021-12-17 10:27:15 -0600

    The answer is postcss

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  syntax: 'postcss-scss',
+
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+}
